46867c02cf79c8e3579cf71bb7d35d4e36bf8a8f
TAIWO KOLAWOLE <kolawole.taiwo@andela.com>
2018-10-31 12:04:40 +0100

    add webpack babel react eslint

---
+++ web/src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <div>
+    <h1>Base Content</h1>
+  </div>,
+  document.getElementById('app'),
+);
+
+if (module.hot) module.hot.accept();
