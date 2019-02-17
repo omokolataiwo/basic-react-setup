84c60dbbda7f39f0bda1a664ebe8d4018b22de40
TAIWO KOLAWOLE <kolawole.taiwo@andela.com>
2019-02-17 19:50:28 +0100

    update test

--- web/__tests__/components/App.test.jsx
+++ web/__tests__/components/App.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import { App } from '../../src/App';
+import App from '../../src/App';
 
 const props = {
   postMessage: jest.fn(() => {})
@@ -10,23 +10,6 @@ const wrapper = mount(<App {...props} />);
 
 describe('App Component', () => {
   it('should render component', () => {
-    expect(wrapper.find('h3').text()).toEqual('Simple React App');
-  });
-  it('should make post message clickable', () => {
-    const postMessageBtn = wrapper.find('#post-message-btn');
-    postMessageBtn.simulate('click');
-    expect(props.postMessage).toHaveBeenCalled();
-  });
-
-  it('should submit form', () => {
-    const messageForm = wrapper.find('#message-form');
-    messageForm.simulate('submit');
-    expect(props.postMessage).toHaveBeenCalled();
-  });
-
-  it('should be able to edit message', () => {
-    const messageField = wrapper.find('#message');
-    messageField.simulate('change', { target: { value: 'Hello, Mr. Green.' } });
-    expect(wrapper.state().message).toEqual('Hello, Mr. Green.');
+    expect(wrapper).toMatchSnapshot();
   });
 });
